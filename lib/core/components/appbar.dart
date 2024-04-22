fc1cd7328a0b1e40570d17357e0d02c3806623ba
fatihkurtl <kurtf061@gmail.com>
2024-04-22 17:29:59 +0300

    Refactor appbar and home view styles, fix import statements, and add recipe model

--- lib/core/components/appbar.dart
+++ lib/core/components/appbar.dart
@@ -1,5 +1,5 @@
 import "package:flutter/material.dart";
-import "package:google_fonts/google_fonts.dart";
+// import "package:google_fonts/google_fonts.dart";
 
 class CustomAppBar extends StatefulWidget {
   const CustomAppBar({super.key});
@@ -12,20 +12,12 @@ class _CustomAppBarState extends State<CustomAppBar> {
   @override
   Widget build(BuildContext context) {
     return AppBar(
-      backgroundColor: Colors.grey[300],
-      title: Text(
+      title: const Text(
         "TARİFİM OLSUN",
-        style: TextStyle(
-          color: Colors.black,
-          fontSize: 24,
-          fontWeight: FontWeight.bold,
-          fontFamily: GoogleFonts.bebasNeue().fontFamily,
-        ),
       ),
       leading: IconButton(
-        icon: Icon(
+        icon: const Icon(
           Icons.menu,
-          color: Colors.grey[600],
         ),
         onPressed: () {
           Scaffold.of(context).openDrawer();
--- lib/core/theme/app_theme.dart
+++ lib/core/theme/app_theme.dart
@@ -1,3 +1,23 @@
-// import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class AppTheme {}
+
+final ThemeData appThemeData = ThemeData(
+  appBarTheme: AppBarTheme(
+    backgroundColor: Colors.grey[300],
+    centerTitle: true,
+    titleTextStyle: TextStyle(
+      color: Colors.black,
+      fontSize: 24,
+      fontWeight: FontWeight.bold,
+      fontFamily: GoogleFonts.bebasNeue().fontFamily,
+    ),
+    iconTheme: IconThemeData(
+      color: Colors.grey[600],
+    ),
+  ),
+  bottomAppBarTheme: BottomAppBarTheme(
+    color: Colors.grey[300],
+  ),
+);
